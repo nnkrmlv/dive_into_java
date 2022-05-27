5c7f8ddec0651b654ca9d51d9ae7763035a4c6da
Aleksandr Komarov <aleksandr.komarov@nordigy.ru>
2022-05-27 16:24:54 +0400

    add stack task

---
+++ src/main/java/learn/Stack.java
@@ -0,0 +1,22 @@
+
+// TODO: implement stack and write 5 tests for it
+/**
+ * usage example:
+ *         Stack stack = new Stack();
+ *         stack.push("aaa");
+ *         String str = stack.pop();
+ *         System.out.println("test1: " + str.equals("aaa"));
+ */
+class Stack {
+
+    public void push(String element) {
+        // TODO: implement
+    }
+
+    public String pop() {
+        // TODO: implement
+        return null;
+    }
+
+
+}
